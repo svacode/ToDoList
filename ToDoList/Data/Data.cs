aa5b8a513f9ca000964952cb74fdbaee1afcccc3
sevam <sevam@SEVA>
2024-10-28 03:59:05 +0400

    created db

--- ToDoList/Data/Data.cs
+++ ToDoList/Data/Data.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Entities;
 
 namespace ToDoList.Data
 {
     public class DataContext : DbContext
     {
 
-
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        { 
+        }
+        public DbSet<ToDo> ToDos { get; set; }
     }
 }
