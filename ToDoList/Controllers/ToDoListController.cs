964fbb29c5b5b0c8ed4605d47d6afe8cdbf75c29
sevam <sevam@SEVA>
2024-10-28 01:09:01 +0400

    Add project files.

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListController : ControllerBase
+    {
+
+    }
+}
