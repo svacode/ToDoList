a064f966279e4a163367383bf158cf98b05b90de
sevam <sevam@SEVA>
2024-10-29 01:09:33 +0400

    updated ToDo class

--- ToDoList/Entities/ToDo.cs
+++ ToDoList/Entities/ToDo.cs
@@ -1,21 +1,22 @@
-﻿namespace ToDoList.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace ToDoList.Entities
 {
     public class ToDo
     {
-
+        [JsonIgnore]
         public int Id { get; set; }
         public required string Timeline { get; set; } 
-        public required string One { get; set; } 
+        public required string Header { get; set; } 
 
-        public string Two { get; set; } = string.Empty ;
+        public string Description { get; set; } = string.Empty ;
 
-        public string Three { get; set; } = string.Empty;
+        public string TimeToFinish { get; set; } = string.Empty;
 
-        public string Four { get; set; } = string.Empty;
-        public string Five { get; set; } = string.Empty;
-        public string Six { get; set; } = string.Empty;
-        public string Seven { get; set; } = string.Empty;
-        public string Eight { get; set; } = string.Empty;
+        public string Deadline { get; set; } = string.Empty;
+
+        public bool Done;
+
 
         
 
